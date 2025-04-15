a67abefdbf43e5297641f746e69f285576604d22
etorrey <elena_torrey@urmc.rochester.edu>
2025-04-15 12:42:52 -0400

    added tokens

---
+++ custom/publish_custom.php
@@ -0,0 +1,45 @@
+<?php
+// Cascade CMS SOAP WSDL URL
+$soapURL = "https://its-wcms-up01.its.rochester.edu:8443/ws/services/AssetOperationService?wsdl";
+
+// Authentication Details
+$auth = array(
+    'apiKey'   => 'c58322bf-0b6d-4156-a316-3c7b084a62dc'      // Replace with your API key
+);
+
+// File Details
+$siteName = "SON-sandbox";           // Replace with the exact site name
+$cssFilePath = "/assets/css/menu.css"; // Path to the CSS file in Cascade CMS
+$localCssPath = "menu.css";  // Path to the local CSS file
+
+
+    $client = new SoapClient($soapURL, array('trace' => 1, 'location' => str_replace('?wsdl', '', $soapURL)));
+
+ 
+    $identifier = array(
+        'path' => array('path' => $cssFilePath, 'siteName' => $siteName),
+        'type' => 'file'
+    );
+
+    $destinationIdentifier = array(
+	// ID or path of the destination
+	'id' => '420398edac19047648ae545a5873e375',
+	'type' => 'destination'
+    );
+
+
+    $publishInformation = array
+(
+	'identifier' => $identifier,
+ 	'destinations' => array ($destinationIdentifier), // This is optional, not providing this will result in publishing to all enabled destinations available to authenticating user 
+	'unpublish' => false // This is optional, default is false
+);
+
+$publishParams = array ('authentication' => $auth, 'publishInformation' => $publishInformation); 
+$reply = $client->publish($publishParams);
+
+if ($reply->publishReturn->success=='true')
+	echo "Success: Published.";
+else
+	echo "Error occurred when publishing: " . $reply->publishReturn->message;
+?>
