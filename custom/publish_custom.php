55536a0c62f830967d938b493995eefbf9421b25
etorrey <elena_torrey@urmc.rochester.edu>
2025-04-16 13:48:06 -0400

    some more changes april 16

--- custom/publish_custom.php
+++ custom/publish_custom.php
@@ -9,8 +9,8 @@ $auth = array(
 
 // File Details
 $siteName = "SON-sandbox";           // Replace with the exact site name
-$cssFilePath = "/assets/css/menu.css"; // Path to the CSS file in Cascade CMS
-$localCssPath = "menu.css";  // Path to the local CSS file
+$cssFilePath = "/assets/css/custom.css"; // Path to the CSS file in Cascade CMS
+$localCssPath = "custom.css";  // Path to the local CSS file
 
 
     $client = new SoapClient($soapURL, array('trace' => 1, 'location' => str_replace('?wsdl', '', $soapURL)));
